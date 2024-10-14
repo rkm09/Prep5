fe089a6b7cbee7fa3523c54adcfc0db0895adaaa
rkm09 <raksha.mishra09@gmail.com>
2024-10-14 22:47:41 +0530

    max k elements

---
+++ src/main/java/dailychallenge/medium/MaxKElements2530.java
@@ -0,0 +1,51 @@
+package dailychallenge.medium;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class MaxKElements2530 {
+    public static void main(String[] args) {
+        int[] nums = {1,10,3,3,3};
+        System.out.println(maxKelements(nums, 3));
+    }
+
+//    priority queue; time: O(klogn + nlogn), space: O(n)
+    public static long maxKelements(int[] nums, int k) {
+        long score = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int num : nums)
+            pq.add(num);
+        while(k-- > 0 && !pq.isEmpty()) {
+            score += pq.peek();
+            pq.add((int)Math.ceil(pq.poll()/3.0));
+        }
+        return score;
+    }
+}
+
+/*
+You are given a 0-indexed integer array nums and an integer k. You have a starting score of 0.
+In one operation:
+choose an index i such that 0 <= i < nums.length,
+increase your score by nums[i], and
+replace nums[i] with ceil(nums[i] / 3).
+Return the maximum possible score you can attain after applying exactly k operations.
+The ceiling function ceil(val) is the least integer greater than or equal to val.
+
+Example 1:
+Input: nums = [10,10,10,10,10], k = 5
+Output: 50
+Explanation: Apply the operation to each array element exactly once. The final score is 10 + 10 + 10 + 10 + 10 = 50.
+Example 2:
+Input: nums = [1,10,3,3,3], k = 3
+Output: 17
+Explanation: You can do the following operations:
+Operation 1: Select i = 1, so nums becomes [1,4,3,3,3]. Your score increases by 10.
+Operation 2: Select i = 1, so nums becomes [1,2,3,3,3]. Your score increases by 4.
+Operation 3: Select i = 2, so nums becomes [1,1,1,3,3]. Your score increases by 3.
+The final score is 10 + 4 + 3 = 17.
+
+Constraints:
+1 <= nums.length, k <= 105
+1 <= nums[i] <= 109
+ */
