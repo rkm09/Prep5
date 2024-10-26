da96e2b455407a6662001aade4a06a9c38d277f9
rkm09 <raksha.mishra09@gmail.com>
2024-10-26 22:28:04 +0530

    tree queries

---
+++ src/main/java/dailychallenge/hard/TreeQueries2458.java
@@ -0,0 +1,86 @@
+package dailychallenge.hard;
+
+import common.TreeNode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TreeQueries2458 {
+    Map<Integer, Integer> resultMap;
+    Map<TreeNode, Integer> heightCache;
+    public static void main(String[] args) {
+        TreeQueries2458 t = new TreeQueries2458();
+        int[] queries = {1};
+        TreeNode root = new TreeNode(1, null, null);
+        int[] res = t.treeQueries(root, queries);
+        System.out.println(Arrays.toString(res));
+    }
+
+//    single traversal; dfs; time: O(n), space: O(n)
+    public int[] treeQueries(TreeNode root, int[] queries) {
+        resultMap = new HashMap<>();
+        heightCache = new HashMap<>();
+//      run dfs to fill result map with maximum height after each query
+        dfs(root, 0, 0);
+        int[] result = new int[queries.length];
+        for(int i = 0 ; i < queries.length ; i++)
+            result[i] = resultMap.get(queries[i]);
+        return result;
+    }
+
+//    calculate height of the tree
+    private int getHeight(TreeNode node) {
+        if(node == null) return -1;
+//        return cached height if already calculated
+        if(heightCache.containsKey(node))
+            return heightCache.get(node);
+        int height = 1 + Math.max(getHeight(node.left), getHeight(node.right));
+        heightCache.put(node, height);
+        return height;
+    }
+
+//    dfs to compute the maximum values after removing the subtree
+    private void dfs(TreeNode node, int depth, int maxVal) {
+        if(node == null) return;
+        resultMap.put(node.val, maxVal);
+//        traverse left and right sub trees while updating the maxVal
+        dfs(node.left, depth + 1, Math.max(maxVal, depth + 1 + getHeight(node.right)));
+        dfs(node.right, depth + 1, Math.max(maxVal, depth + 1 + getHeight(node.left)));
+    }
+}
+
+/*
+You are given the root of a binary tree with n nodes. Each node is assigned a unique value from 1 to n. You are also given an array queries of size m.
+You have to perform m independent queries on the tree where in the ith query you do the following:
+Remove the subtree rooted at the node with the value queries[i] from the tree. It is guaranteed that queries[i] will not be equal to the value of the root.
+Return an array answer of size m where answer[i] is the height of the tree after performing the ith query.
+Note:
+The queries are independent, so the tree returns to its initial state after each query.
+The height of a tree is the number of edges in the longest simple path from the root to some node in the tree.
+
+Example 1:
+Input: root = [1,3,4,2,null,6,5,null,null,null,null,null,7], queries = [4]
+Output: [2]
+Explanation: The diagram above shows the tree after removing the subtree rooted at node with value 4.
+The height of the tree is 2 (The path 1 -> 3 -> 2).
+Example 2:
+Input: root = [5,8,9,2,1,3,7,4,6], queries = [3,2,4,8]
+Output: [3,2,3,2]
+Explanation: We have the following queries:
+- Removing the subtree rooted at node with value 3. The height of the tree becomes 3 (The path 5 -> 8 -> 2 -> 4).
+- Removing the subtree rooted at node with value 2. The height of the tree becomes 2 (The path 5 -> 8 -> 1).
+- Removing the subtree rooted at node with value 4. The height of the tree becomes 3 (The path 5 -> 8 -> 2 -> 6).
+- Removing the subtree rooted at node with value 8. The height of the tree becomes 2 (The path 5 -> 9 -> 3).
+
+Constraints:
+The number of nodes in the tree is n.
+2 <= n <= 105
+1 <= Node.val <= n
+All the values in the tree are unique.
+m == queries.length
+1 <= m <= min(n, 104)
+1 <= queries[i] <= n
+queries[i] != root.val
+
+ */
--- src/main/java/dailychallenge/medium/RemoveSubfolders1233.java
+++ src/main/java/dailychallenge/medium/RemoveSubfolders1233.java
@@ -1,8 +1,6 @@
 package dailychallenge.medium;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class RemoveSubfolders1233 {
     public static void main(String[] args) {
@@ -10,8 +8,32 @@ public class RemoveSubfolders1233 {
         System.out.println(removeSubfolders(folder));
     }
 
-//    sorting; time: O(N.LlogN), space: O(logn) [N - number of folders, L - maximum length of a folder path]
+//    trie; time: O(N.L), space: O(N.L)
+//    A Trie is well-suited for this problem because it allows us to build folder paths incrementally,
+//    marking endpoints where folders end. With this structure, any folder that tries to extend beyond an
+//    endpoint can be identified as a sub-folder.
     public static List<String> removeSubfolders(String[] folder) {
+        TrieNode root = new TrieNode();
+        List<String> result = new ArrayList<>();
+//        build trie from folder paths
+        for(String path : folder) {
+
+        }
+        return result;
+    }
+
+    static class TrieNode {
+        boolean isEndOfFolder;
+        Map<String, TrieNode> children;
+        TrieNode() {
+            isEndOfFolder = false;
+            children = new HashMap<>();
+        }
+    }
+
+//    sorting; time: O(N.LlogN), space: O(logn) [N - number of folders, L - maximum length of a folder path]
+//    Sorting takes O(N⋅logN) comparisons, but each comparison can involve up to L characters (the maximum length of a folder path).
+    public static List<String> removeSubfolders1(String[] folder) {
 //        sort the folders alphabetically
         Arrays.sort(folder);
 //        initialize the result list and add the first folder
@@ -26,6 +48,44 @@ public class RemoveSubfolders1233 {
                 result.add(folder[i]);
         }
 //        return the result containing only non sub folders
+        return result;
+    }
+
+//    using set; time: O(N.L^2), space: O(N.L)
+//    Constructing the unordered set folderSet from the input array folder takes O(N).
+//    However, each string insertion requires O(L). So, initializing the set takes O(N⋅L).
+//    The primary operation involves iterating over each folder path in the folder array, which is O(N).
+//    For each folder, the algorithm checks all possible prefixes (up to L levels deep) in the folderSet.
+//    This involves: Finding the position of the last '/' character in the prefix string, which takes O(L)
+//    in the worst case. Creating a substring for each prefix level, which is also O(L). Searching for each
+//    prefix in the set, which is O(L). Therefore, checking all prefixes of one folder takes O(L^2), and for N
+//    folders, this results in O(N⋅L^2).
+    public static List<String> removeSubfolders2(String[] folder) {
+//        create a set to store all folder paths for fast lookup
+        Set<String> folderSet = new HashSet<>(Arrays.asList(folder));
+        List<String> result = new ArrayList<>();
+
+//        iterate through each folder to check if it's a sub folder
+        for(String path  : folder) {
+            boolean isSubFolder = false;
+            String prefix = path;
+//            check all prefixes of the current folder path
+            while(!prefix.isEmpty()) {
+                int pos = prefix.lastIndexOf("/");
+                if(pos == -1) break;
+//                reduce the prefix to it's parent folder
+                prefix = prefix.substring(0, pos);
+//                if the parent folder exists in the set, mark it as a sub folder
+                if(folderSet.contains(prefix)) {
+                    isSubFolder = true;
+                    break;
+                }
+            }
+//            if not, add it to the result
+            if(!isSubFolder)
+                result.add(path);
+        }
+
         return result;
     }
 }
