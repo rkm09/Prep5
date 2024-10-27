7a181fda2491aeb799d8040341466e7b749df561
rkm09 <raksha.mishra09@gmail.com>
2024-10-27 14:54:33 +0530

    count squares

---
+++ src/main/java/dailychallenge/medium/CountSquares1277.java
@@ -0,0 +1,60 @@
+package dailychallenge.medium;
+
+public class CountSquares1277 {
+    public static void main(String[] args) {
+        int[][] matrix = {{0,1,1,1},{1,1,1,1},{0,1,1,1}};
+        System.out.println(countSquares(matrix));
+    }
+
+//    bottom up dp; time: O(m*n), space: O(m*n)
+    public static int countSquares(int[][] matrix) {
+        int row = matrix.length, col = matrix[0].length;
+        int[][] dp = new int[row + 1][col + 1];
+        int ans = 0;
+        for(int i = 0 ; i < row ; i++) {
+            for(int j = 0 ; j < col ; j++) {
+                if(matrix[i][j] == 1) {
+                    dp[i + 1][j + 1] = Math.min(matrix[i + 1][j + 1],
+                            Math.min(matrix[i + 1][j], matrix[i][j + 1])) + 1;
+                }
+                ans += dp[i + 1][j + 1];
+            }
+        }
+        return ans;
+    }
+}
+
+/*
+Given an m * n matrix of ones and zeros, return how many square sub matrices have all ones.
+
+Example 1:
+Input: matrix =
+[
+  [0,1,1,1],
+  [1,1,1,1],
+  [0,1,1,1]
+]
+Output: 15
+Explanation:
+There are 10 squares of side 1.
+There are 4 squares of side 2.
+There is  1 square of side 3.
+Total number of squares = 10 + 4 + 1 = 15.
+Example 2:
+Input: matrix =
+[
+  [1,0,1],
+  [1,1,0],
+  [1,1,0]
+]
+Output: 7
+Explanation:
+There are 6 squares of side 1.
+There is 1 square of side 2.
+Total number of squares = 6 + 1 = 7.
+
+Constraints:
+1 <= arr.length <= 300
+1 <= arr[0].length <= 300
+0 <= arr[i][j] <= 1
+ */
