f2c90969d7f184d20e92bfc9f1a196a8a3395428
rkm09 <raksha.mishra09@gmail.com>
2024-10-27 14:57:20 +0530

    count squares

--- src/main/java/dailychallenge/medium/CountSquares1277.java
+++ src/main/java/dailychallenge/medium/CountSquares1277.java
@@ -14,8 +14,8 @@ public class CountSquares1277 {
         for(int i = 0 ; i < row ; i++) {
             for(int j = 0 ; j < col ; j++) {
                 if(matrix[i][j] == 1) {
-                    dp[i + 1][j + 1] = Math.min(matrix[i + 1][j + 1],
-                            Math.min(matrix[i + 1][j], matrix[i][j + 1])) + 1;
+                    dp[i + 1][j + 1] = Math.min(dp[i][j],
+                            Math.min(dp[i + 1][j], dp[i][j + 1])) + 1;
                 }
                 ans += dp[i + 1][j + 1];
             }
