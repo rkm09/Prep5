84dc3f4ab9f942b40b62de0c10639960c21935ff
rkm09 <raksha.mishra09@gmail.com>
2024-11-24 22:51:15 +0530

    all one

--- src/main/java/prep/AllOne432.java
+++ src/main/java/prep/AllOne432.java
@@ -1,49 +1,123 @@
 package prep;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class AllOne432 {
 }
 
 class AllOne {
-    Map<String, Integer> freqMap;
+    Node head;
+    Node tail;
+    Map<String, Node> map;
     public AllOne() {
-        freqMap = new HashMap<>();
+        head = new Node(0);
+        tail = new Node(0);
+        map = new HashMap<>();
+        head.next = tail;
+        tail.prev = head;
     }
 
     public void inc(String key) {
-        freqMap.put(key, freqMap.getOrDefault(key, 0) + 1);
+        if(map.containsKey(key)) {
+            Node node = map.get(key);
+            int freq = node.freq;
+            node.keys.remove(key);
+
+            Node nextNode = node.next;
+            if(nextNode == tail || nextNode.freq != freq + 1) {
+                Node newNode = new Node(freq + 1);
+                newNode.keys.add(key);
+                newNode.prev = node;
+                newNode.next = nextNode;
+                node.next = newNode;
+                nextNode.prev = newNode;
+                map.put(key, newNode);
+            } else {
+                nextNode.keys.add(key);
+                map.put(key, nextNode);
+            }
+
+            if(node.keys.isEmpty())
+                remove(node);
+
+        } else {
+            Node firstNode = head.next;
+            if(firstNode == tail || firstNode.freq > 1) {
+                Node newNode = new Node(1);
+                newNode.next = firstNode;
+                newNode.prev = head;
+                head.next = newNode;
+                firstNode.prev = newNode;
+                newNode.keys.add(key);
+                map.put(key, newNode);
+            } else {
+                firstNode.keys.add(key);
+                map.put(key, firstNode);
+            }
+        }
     }
 
     public void dec(String key) {
-        freqMap.put(key, freqMap.get(key) - 1);
+        if(!map.containsKey(key)) return;
+        Node node = map.get(key);
+        node.keys.remove(key);
+        int freq = node.freq;
+        if(freq == 1) {
+            map.remove(key);
+        } else {
+            Node prevNode = node.prev;
+            if(prevNode == head || prevNode.freq != freq - 1) {
+                Node newNode = new Node(freq - 1);
+                newNode.keys.add(key);
+                map.put(key, newNode);
+                newNode.prev = prevNode;
+                newNode.next = node;
+                prevNode.next = newNode;
+                node.prev = newNode;
+            } else {
+                prevNode.keys.add(key);
+                map.put(key, prevNode);
+            }
+        }
+        if(node.keys.isEmpty())
+            remove(node);
     }
 
     public String getMaxKey() {
-        int max = 0;
-        for(String key : freqMap.keySet()) {
-            max = Math.max(max, freqMap.get(key));
-        }
-        for(String key : freqMap.keySet()) {
-            if(freqMap.get(key) == max)
-                return key;
-        }
-        return "";
+        if(tail.prev == head)
+            return "";
+        return tail.prev.keys.iterator().next();
     }
 
     public String getMinKey() {
-        int min = Integer.MAX_VALUE;
-        for(String key : freqMap.keySet()) {
-            min = Math.min(min, freqMap.get(key));
+        if(head.next == tail)
+            return "";
+        return head.next.keys.iterator().next();
+    }
+
+    public void remove(Node node) {
+        Node prevNode = node.prev;
+        Node nextNode = node.next;
+        prevNode.next = nextNode;
+        nextNode.prev = prevNode;
+    }
+
+    class Node {
+        int freq;
+        Node next;
+        Node prev;
+        Set<String> keys;
+        Node(int freq) {
+            this.freq = freq;
+            keys = new HashSet<>();
         }
-        for(String key : freqMap.keySet()) {
-            if(freqMap.get(key) == min)
-                return key;
-        }
-        return "";
     }
 }
+
+
 
 /**
  * Your AllOne object will be instantiated and called as such:
