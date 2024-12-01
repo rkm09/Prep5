ccdd719637fa3e06daadb63ef96c863d15d9212a
rkm09 <raksha.mishra09@gmail.com>
2024-12-01 17:49:46 +0530

    check exists

--- src/main/java/dailychallenge/easy/CheckIfExists1346.java
+++ src/main/java/dailychallenge/easy/CheckIfExists1346.java
@@ -8,8 +8,62 @@ public class CheckIfExists1346 {
         System.out.println(checkIfExist(arr));
     }
 
-//    def; time: O(nlogn), space: O(n)
+//    hashset; time: O(n), space: O(n)
     public static boolean checkIfExist(int[] arr) {
+        Set<Integer> seen = new HashSet<>();
+        for(int num : arr) {
+            if(seen.contains(num * 2) || ((num % 2 == 0) && seen.contains(num / 2)))
+                return true;
+            seen.add(num);
+        }
+        return false;
+    }
+
+//    frequency map; time: O(n), space: O(n)
+//    since this problem allows negative numbers, a frequency map is a better choice compared to freq array
+    public static boolean checkIfExist1(int[] arr) {
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for(int num : arr)
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        for(int num : arr) {
+            if(num != 0 && freqMap.containsKey(num * 2))
+                return true;
+            if(num == 0 && freqMap.get(num) > 1)
+                return true;
+        }
+        return false;
+    }
+
+
+//    sorting + binary search; time: O(nlogn), space: O(n)
+    public static boolean checkIfExist2(int[] arr) {
+        Arrays.sort(arr);
+        for(int i = 0 ; i < arr.length ; i++) {
+            int target = arr[i] * 2;
+            int index = binarySearch(arr, target);
+            if(index >= 0 && index != i) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static int binarySearch(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if(arr[mid] == target)
+                return mid;
+            if(arr[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return -1;
+    }
+
+//    def; time: O(nlogn), space: O(n)
+    public static boolean checkIfExist3(int[] arr) {
         Set<Integer> set = new HashSet<>();
         List<Integer> li = new ArrayList<>();
         for (int j : arr) li.add(j);
