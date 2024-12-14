cd453171f74bb4a1a70ee0de86eb3391b0ffd340
rkm09 <raksha.mishra09@gmail.com>
2024-12-14 08:11:19 +0530

    find score

--- src/main/java/dailychallenge/medium/FindScore2593.java
+++ src/main/java/dailychallenge/medium/FindScore2593.java
@@ -1,13 +1,70 @@
 package dailychallenge.medium;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class FindScore2593 {
     public static void main(String[] args) {
         int[] nums = {2,1,3,4,5,2};
-        System.out.println(findScore(nums));
+        int[] nums1 = {2,5,6,6,10}; //18
+        System.out.println(findScore1(nums1));
     }
 
+//    sorting; time: O(nlogn), space: O(n)
     public static long findScore(int[] nums) {
-        
+        int n = nums.length;
+        int[][] sorted = new int[n][2];
+        boolean[] marked = new boolean[n];
+        long score = 0;
+        for(int i = 0 ; i < n ; i++) {
+            sorted[i][0] = nums[i];
+            sorted[i][1] = i;
+        }
+        Arrays.sort(sorted, Comparator.comparingInt(a -> a[0]));
+        for(int i = 0 ; i < n ; i++) {
+            int number = sorted[i][0];
+            int index = sorted[i][1];
+            if(!marked[index]) {
+                score += number;
+                marked[index] = true;
+//                mark adjacent elements if they exist
+                if(index + 1 < n)
+                    marked[index + 1] = true;
+                if(index - 1 >= 0)
+                    marked[index - 1] = true;
+            }
+        }
+        return score;
+    }
+
+//    min heap; time: O(nlogn), space: O(n)
+    public static long findScore1(int[] nums) {
+        int n = nums.length;
+//        note that unlike the previous one, pq may shuffle again after each poll, hence important to sort based on 2 conditions
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> {
+            if(a[0] != b[0]) return a[0] - b[0];
+            else return a[1] - b[1];
+        });
+        boolean[] marked = new boolean[n];
+        for(int i = 0 ; i < n ; i++) {
+            pq.offer(new int[]{nums[i],i});
+        }
+        long score = 0;
+        while(!pq.isEmpty()) {
+            int[] numberInfo = pq.poll();
+            int number = numberInfo[0];
+            int index = numberInfo[1];
+            if(!marked[index]) {
+                score += number;
+                marked[index] = true;
+                if(index - 1 >= 0)
+                    marked[index - 1] = true;
+                if(index + 1 < n)
+                    marked[index + 1] = true;
+            }
+        }
+        return score;
     }
 }
 
