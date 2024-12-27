17ae39d56dbd1ae06e8920b472ca4a3fe7c7aeee
rkm09 <raksha.mishra09@gmail.com>
2024-12-27 06:41:27 +0530

    maximal length

--- src/main/java/dailychallenge/medium/MaximalLength2832.java
+++ src/main/java/dailychallenge/medium/MaximalLength2832.java
@@ -1,12 +1,37 @@
 package dailychallenge.medium;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class MaximalLength2832 {
     public static void main(String[] args) {
 
     }
 
+//    monotonic stack; time: O(n), space: O(n)
     public static int[] maximumLengthOfRanges(int[] nums) {
-
+        int n = nums.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        Deque<Integer> stack = new ArrayDeque<>();
+        for(int i = 0 ; i < n ; i++) {
+            while(!stack.isEmpty() && nums[stack.peek()] < nums[i])
+                stack.pop();
+            left[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(i);
+        }
+        stack.clear();
+        for(int i = n - 1 ; i >= 0 ; i--) {
+            while(!stack.isEmpty() && nums[stack.peek()] < nums[i])
+                stack.pop();
+            right[i] = stack.isEmpty() ? n : stack.peek();
+            stack.push(i);
+        }
+        int[] ans = new int[n];
+        for(int i = 0 ; i < n ; i++) {
+            ans[i] = right[i] - left[i] - 1;
+        }
+        return ans;
     }
 }
 
@@ -34,4 +59,13 @@ Constraints:
 1 <= nums.length <= 105
 1 <= nums[i] <= 105
 All elements in nums are distinct.
+ */
+
+/*
+Since nums contains distinct elements, it is guaranteed that each element will be the maximum in at least one subarray.
+What’s the smallest possible size of a subarray where the current element is the maximum? The answer is 1, because the element itself is a subarray of size 1. Since we want to find the longest subarray that contains the current element, we can start adding elements from the left or the right side of the current element. Now, let's consider the elements to the left of the current element:
+
+If these elements are smaller than the current element, they can be included in the subarray.
+However, as soon as we encounter an element larger than the current one, we must stop, as the current element can no longer be the maximum in the subarray.
+The same logic applies to the elements on the right side.
  */
